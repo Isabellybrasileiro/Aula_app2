1a0eb7ea3915a6c642bb7c325a35efb1b0e8e068
ISABELLY BRASILEIRO <09477318906@educacional.up>
2022-08-08 11:46:18 -0300

    Initial commit

---
+++ app/src/main/java/br/com/up/projeto1/MainActivity.java
@@ -0,0 +1,43 @@
+package br.com.up.projeto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSelector;
+    private TextView textViewTitle;
+    private ArrayList<String> listPhases = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listPhases.add("Comprar um café para o professor");
+        listPhases.add("Chamar o prof para um churras");
+
+        buttonSelector = findViewById(R.id.button_selector);
+        textViewTitle = findViewById(R.id.text_title);
+
+        View.OnClickListener clickListener =
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        int position = new Random().nextInt(listPhases.size());
+                        String text = listPhases.get(position);
+                        textViewTitle.setText(text);
+                    }
+                };
+
+        buttonSelector.setOnClickListener(clickListener);
+    }
+}
